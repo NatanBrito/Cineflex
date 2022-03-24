4b0b01ccc5ab3e7c59f548267480f8f09a8e5118
Natan__Brito <natanismaelbrito@gmail.com>
2022-03-24 12:42:02 -0300

    falta só o botão da tela 3

--- src/components/tela3/Tela3.js
+++ src/components/tela3/Tela3.js
@@ -1,30 +1,17 @@
 import "./style.css";
 import Footer from "../footer/Footer";
 export default function Tela3(){
+    const testes=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+        ,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
     return(
         <>
         <div className="tituloSelecao">Selecione o horário</div>
         <div className="assentos">
-            <div className="assento">1</div>
-            <div className="assento">2</div>
-            <div className="assento">3</div>
-            <div className="assento">4</div>
-            <div className="assento">5</div>
-            <div className="assento">6</div>
-            <div className="assento">7</div>
-            <div className="assento">8</div>
-            <div className="assento">9</div>
-            <div className="assento">10</div>
-            <div className="assento">11</div>
-            <div className="assento">12</div>
-            <div className="assento">13</div>
-            <div className="assento">14</div>
-            <div className="assento">15</div>
-            <div className="assento">16</div>
-            <div className="assento">17</div>
-            <div className="assento">18</div>
-            <div className="assento">19</div>
-            <div className="assento">20</div>
+           { testes.map(teste =>{
+                return(
+                    <div className="assento">{teste}</div>
+                )
+            })}
 
         </div>
         <div className="disposicao">
@@ -36,6 +23,14 @@ export default function Tela3(){
         <span className="estadoAssento">Selecionado</span>
         <span className="estadoAssento">Disponivel</span>
         <span className="estadoAssento">Indisponivel</span>
+        </div>
+        <div className="inputs">
+        <span className="titulo">Nome do comprador:</span>
+        <input type="text" className="input" placeholder="   Digite seu nome..."></input>
+        </div>
+        <div className="inputs cpf">
+        <span className="titulo">CPF do comprador:</span>
+        <input type="text" className="input" placeholder="   Digite seu CPF..."></input>
         </div>
         <Footer />
         </>
